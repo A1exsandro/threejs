6ba42128392a7bad48718a5f4e40374b3aabaabf
A1exsandro <alexsandro.acp@gmail.com>
2023-02-11 18:38:10 -0400

    add planeGeometry

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ const scene = new THREE.Scene()
 
 // SET CAMERA
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -23,7 +23,7 @@ const orbit = new OrbitControls(camera, renderer.domElement)
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-camera.position.set(0, 2, 5)
+camera.position.set(-10, 30, 30)
 orbit.update()
 
 // SET BOX
@@ -31,6 +31,19 @@ const boxGeometry = new THREE.BoxGeometry()
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00})
 const box = new THREE.Mesh(boxGeometry, boxMaterial)
 scene.add(box)
+
+// SET PLANE
+const planeGeometry = new THREE.PlaneGeometry(30, 30)
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xFFFFFF,
+  side: THREE.DoubleSide
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(plane)
+plane.rotation.x = -0.5 * Math.PI
+
+const gridHelper = new THREE.GridHelper(30)
+scene.add(gridHelper)
 
 function animate(time) {
   box.rotation.x = time / 1000
